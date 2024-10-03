c2123cbbd2a24b619d3defbef62588910bb048aa
Vivek <20bcs105@iiitdwd.ac.in>
2024-10-03 14:37:19 +0530

    revert to externstion

--- extenstion/vite.config.js
+++ extenstion/vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa';
+// import { VitePWA } from 'vite-plugin-pwa';
 import manifest from "./manifest.json";
-// import { crx } from "@crxjs/vite-plugin";
+import { crx } from "@crxjs/vite-plugin";
 
 
 export default defineConfig({
@@ -11,24 +11,25 @@ export default defineConfig({
   },
   plugins: [
     react(),
-    VitePWA({
-      registerType: 'autoUpdate',
-      manifest:manifest,
-      workbox: {
-        runtimeCaching: [
-          {
-            urlPattern: /^https:\/\/api\.example\.com\/.*/, // Example API URL
-            handler: 'NetworkFirst', // Cache data while also trying the network
-            options: {
-              cacheName: 'api-cache',
-              expiration: {
-                maxEntries: 50,
-                maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
-              },
-            },
-          },
-        ],
-      },
-    }),
+    // VitePWA({
+    //   registerType: 'autoUpdate',
+    //   manifest:manifest,
+    //   workbox: {
+    //     runtimeCaching: [
+    //       {
+    //         urlPattern: /^https:\/\/api\.example\.com\/.*/, // Example API URL
+    //         handler: 'NetworkFirst', // Cache data while also trying the network
+    //         options: {
+    //           cacheName: 'api-cache',
+    //           expiration: {
+    //             maxEntries: 50,
+    //             maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+    //           },
+    //         },
+    //       },
+    //     ],
+    //   },
+    // }),
+    crx({manifest})
   ],
 });
